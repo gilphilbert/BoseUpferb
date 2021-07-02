641507b636cf78796dcc1b3ef447ff113840d0dd
gilphilbert <gil.philbert@gmail.com>
2021-07-02 14:11:16 -0700

    init

--- src/rftag.cpp
+++ src/rftag.cpp
@@ -10,48 +10,21 @@
 PN532_I2C pn532i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532i2c);
 
-//uint8_t connected = true;
-//int readTimeout = 1000;
-//int timer = 0;
-uint8_t flag = false;
-
-void IRAM_ATTR ISR() {
-  flag = true;
-}
+uint8_t connected = true;
+int readTimeout = 1000;
+int timer = 0;
 
 void Radio::begin() {
   nfc.begin();
-  pinMode(NFC_IRQ, INPUT);
-  attachInterrupt(NFC_IRQ, ISR, FALLING);
 }
 
 String Radio::loop() {
-  /*
   if (connected && millis() > timer) {
     readTimeout = 1000;
     timer = millis() + readTimeout;
     Serial.println("Looking for card...");
     if (nfc.tagPresent(100)) {
       readTimeout = 2000;
-      NfcTag tag = nfc.read();
-      NdefMessage message = tag.getNdefMessage();
-      NdefRecord record = message.getRecord(0);
-      int payloadLength = record.getPayloadLength();
-      byte payload[payloadLength];
-      record.getPayload(payload);
-      String payloadAsString = "";
-      for (int c = 3; c < payloadLength; c++) {
-        payloadAsString += (char)payload[c];
-      }
-      return payloadAsString;
-    }
-  }
-  return "";
-  */
-  if (flag) {
-    flag = false;
-    Serial.println("Reading card...");
-    if (nfc.tagPresent(100)) {
       NfcTag tag = nfc.read();
       NdefMessage message = tag.getNdefMessage();
       NdefRecord record = message.getRecord(0);
